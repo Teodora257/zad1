271129362fd6643fec90a8b632e53ec7efc0e5eb
MEGA <MEGA@gmail.com>
2025-03-21 18:41:30 +0200

    Add project files.

---
+++ zad1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете число N от интервала [10..100]: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (N < 10 || N > 100)
+            {
+                Console.WriteLine("Числото трябва да бъде в интервала [10..100].");
+                return;
+            }
+
+            Random random = new Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                numbers.Add(random.Next(1, 102)); 
+            }
+
+            List<int> evenNumbers = new List<int>();
+            List<int> oddNumbers = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                    evenNumbers.Add(num);
+                else
+                    oddNumbers.Add(num);
+            }
+
+            Console.WriteLine("Четни числа: " + string.Join(" ", evenNumbers));
+
+            oddNumbers.Reverse();
+            Console.WriteLine("Нечетни числа: " + string.Join(" ", oddNumbers));
+        }
+    }
+}
